639213b4bb9f2a5a9277bbf2fd9f0cb124486702
Yuliya_L <ulcha89@mail.ru>
2022-12-08 21:14:31 +0200

    Added task 0,1,3,5,7

---
+++ Sem1Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿ // Считываем данные с консоли
+string? inputLine = Console.ReadLine ();
+
+// Проверяем чтобы данные были не пустыми
+if (inputLine!=null)
+{
+    // создаем массив с днями недели
+    string [] dayOfWeek = new string [7];
+    dayOfWeek [0] = "понедельник";
+    dayOfWeek [1] = "вторник";
+    dayOfWeek [2] = "среда";
+    dayOfWeek [3] = "четверг";
+    dayOfWeek [4] = "пятница";
+    dayOfWeek [5] = "суббота";
+    dayOfWeek [6] = "воскресенье";
+
+    //Парсим введенное число
+    int inputNumber = int.Parse (inputLine);
+
+// Находим название дня недели по введенному номеру
+    string outDayOfWeek = dayOfWeek [inputNumber - 1];
+
+    //выводим данные в консоль
+    Console.WriteLine (outDayOfWeek);
+
+}
---
+++ Sem1Task5/Program.cs
@@ -0,0 +1,26 @@
+﻿// Считываем данные с консоли
+ string? inputLine = Console.ReadLine ();
+
+ //Проверяем чтобы данные были не пустими
+ if (inputLine!=null)
+ {
+    // Парсим введеное число
+    int inputNumber = int.Parse (inputLine);
+
+    int startNumber = inputNumber*(-1);
+
+    //Выходное зачение
+    string outLine = string.Empty;
+
+    while (startNumber<inputNumber)
+    {
+        outLine = outLine + startNumber + ",";
+        startNumber ++;
+        //startNumber=StartNumber+1;
+    }
+    outLine = outLine + inputNumber;
+
+    //выводим данные в консоль
+
+    Console.WriteLine (outLine);
+ }
