2c9ce5a4ba548741cbac367327c8dea6a09b3fbb
Yuliya_L <ulcha89@mail.ru>
2022-12-22 13:59:44 +0200

    Task 39,40,42,44

---
+++ Sem6Task39/Program.cs
@@ -0,0 +1,64 @@
+﻿// Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.) Например:
+// [1 2 3 4 5] -> [5 4 3 2 1] [6 7 3 6] -> [6 3 7 6]
+// Комментарий: эту задачу можно решить 2 способами: 1) менять числа местами в исходном массиве; 2) создать новый массив и в него записать перевёрнутый исходный
+// массив по элементам.
+
+int[] arr = GenArray(20,1,100);
+Console.WriteLine("Исходны массив");
+Print1DArr(arr);
+
+int[] copyArr = SwapNewArray(arr);
+Console.WriteLine("Нивый массив перевернутый SwapNewArray методом");
+Print1DArr(copyArr);
+Console.WriteLine("Исходны массив");
+Print1DArr(arr);
+
+int[] copyArrTwo = SwapArray(arr);
+Console.WriteLine("Исходны массив");
+Print1DArr(arr);
+Console.WriteLine("Исходный массив перевернутый SwapArray методом");
+Print1DArr(copyArrTwo);
+
+// Метод, генерирующий массив
+int[] GenArray(int len, int minValue, int maxValue)
+{
+    int[] arr = new int[len];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return arr;
+}
+
+//Метод, выводящий на печать массив
+void Print1DArr(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+int[] SwapArray(int[] arr)
+{
+    int bufElement = 0;
+    for(int i =0;i<arr.Length/2;i++)
+    {
+        bufElement = arr[arr.Length-1-i];
+        arr[arr.Length-1-i] = arr[i];
+        arr[i] = bufElement;
+    }
+    return arr;
+}
+
+int[] SwapNewArray(int[] arr)
+{
+    int[] outArr = new int[arr.Length];
+    for(int i =0;i<arr.Length;i++)
+    {
+        outArr[i] = arr[arr.Length-1-i];
+    }
+    return outArr;
+}
---
+++ Sem6Task44/Program.cs
@@ -0,0 +1,37 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Например:
+// Если N = 5 -> 0 1 1 2 3 Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+int number = ReadData("Введите число:");
+PrintData(FibNum(number));
+
+//Метод, считывающий данные, введенные пользователем
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Метод, выводящий на печать данные пользователю
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+//Числа Фибоначчи
+string FibNum(int num)
+{
+    string outLine = "0 1";
+    int first = 0;
+    int last = 1;
+    int buf = 0;
+    for (int i = 2; i < num; i++)
+    {
+        outLine = outLine +" "+ (first + last).ToString();
+        buf=last;
+        last=first+last;
+        first=buf;
+    }
+    return outLine;
+}
